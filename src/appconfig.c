33c7e0506489c64cad64f5339865431a60bfb1ea
Elkantor <vic03@live.fr>
2023-12-20 23:34:58 +0100

    Add base for pipeline struct

--- src/appconfig.c
+++ src/appconfig.c
@@ -5,6 +5,7 @@
 #include "engine/backends/graphics/windows/openGL/window.c"
 #include "engine/backends/graphics/openGL/vertex.c"
 #include "engine/backends/graphics/openGL/indexBuffer.c"
+#include "engine/backends/graphics/openGL/pipeline.c"
 
 // Include here the plateform specific implementations
 #include "engine/backends/system/windows/window.c"
---
+++ src/engine/backends/graphics/openGL/pipeline.c
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string.h>
+#include "../../../pipeline.c"
+
+typedef struct pipeline
+{
+    cullMode_t m_cullMode;
+    compareMode_t m_depthMode;
+} piepline_t;
+
+void pipelineInit(pipeline_t* _pipeline)
+{
+    memset(_pipeline, 0, sizeof(*_pipeline));
+
+
+}
