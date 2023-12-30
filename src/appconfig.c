75725045d8b486ccd72b6daed631dc23005a9298
Elkantor <vic03@live.fr>
2023-12-30 11:47:52 +0100

    Add raylib as dependency, and make raylib window a child window in the main app window

--- src/appconfig.c
+++ src/appconfig.c
@@ -7,6 +7,9 @@
 //#include "engine/backends/graphics/openGL/indexBuffer.c"
 //#include "engine/backends/graphics/openGL/pipeline.c"
 
+// Raylib
+#include "../libs/raylib/raylib.h"
+
 // Include here the plateform specific implementations
 #include "engine/backends/system/windows/window.c"
 #include "engine/backends/system/windows/display.c"
--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
 #include "appconfig.c"
 
 // NOTE(Victor): build with mingw on windows:
-// gcc src/main.c -g -lDwmapi -lgdi32 -lopengl32 -lglu32 && ./cv2pdb64.exe a.exe
+// gcc src/main.c -g -Wall -Werror -lDwmapi -lwinmm -lgdi32 -lopengl32 -lraylib -L./libs/raylib && ./cv2pdb64.exe a.exe
 // devenv a.exe (to open visual studio debugger)
 
 // NOTE(Victor): build with zig cc (clang) compiler on windows:
-// zig cc src/main.c -g -lDwmapi -lgdi32 -lopengl32 -lglu32
+// zig cc src/main.c -g -Wall -Werror -lDwmapi -lwinmm -lgdi32 -lopengl32 -lraylib -L./libs/raylib
 // devenv a.exe (to open visual studio debugger)
 
 void windowResize(windowData_t* _window, const int _width, const int _height)
@@ -16,25 +16,48 @@ void windowResize(windowData_t* _window, const int _width, const int _height)
     printf("resized %d, %d\n", _width, _height);
 }
 
+
 void appUpdate(app_t* _app)
 {
+    while (!WindowShouldClose() && _app->m_running) {
+        BeginDrawing();
+        ClearBackground((Color) { 245, 245, 245, 255 });
+        DrawRectangle(0, 0, 900, 900, (Color) { 0, 255, 0, 255 });
+        DrawFPS(100, 100);
+        EndDrawing();
+    }
 }
 
 int appKickstart(int argc, char **argv)
 {
     static app_t app;
     appInit(&app, "My App");
-
+    
     static windowArray_t windows;
     windowArrayInit(&app, &windows);
-
     windowInit(&windows, 0, 1024, 768, "First Window");
-    windowInit(&windows, 0, 1024, 768, "Second Window");
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_BORDERLESS_WINDOWED_MODE | FLAG_WINDOW_UNDECORATED);
+    InitWindow(800, 600, "Raylib Window");
+
+    // Make raylib window as child
+    {
+        SetParent(GetWindowHandle(), windows.m_data[0].m_handle);
+        const LONG nNewStyle = (GetWindowLong(GetWindowHandle(), GWL_STYLE) & ~WS_POPUP) | WS_CHILDWINDOW;
+        SetWindowLong(GetWindowHandle(), GWL_STYLE, nNewStyle);
+        const ULONG_PTR cNewStyle = GetClassLongPtr(GetWindowHandle(), GCL_STYLE) | CS_DBLCLKS;
+        SetClassLongPtr(GetWindowHandle(), GCL_STYLE, cNewStyle);
+        SetWindowPos(GetWindowHandle(), HWND_TOP, 0, 0, 800, 600, SWP_DRAWFRAME);
+    }
 
     const uint64_t stackSize = appStackSizeGet();
     printf("App stack size left: %zu\n", stackSize);
+
     appStart(&app);
 
+
+    // Fermer la fenêtre raylib
+    RL_CloseWindow();
 
     return 0;
 }
