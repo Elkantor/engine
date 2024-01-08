ad7f755ee6dc4871beb62199522a3855c5a1d1c6
Elkantor <vic03@live.fr>
2024-01-08 19:56:48 +0100

    Update raylib by adding custom function to draw model with a custom material

--- libs/raylib/raylib.h
+++ libs/raylib/raylib.h
@@ -1527,6 +1527,7 @@ RLAPI BoundingBox GetModelBoundingBox(Model model);                             
 // Model drawing functions
 RLAPI void DrawModel(Model model, Vector3 position, float scale, Color tint);               // Draw a model (with texture if set)
 RLAPI void DrawModelEx(Model model, Vector3 position, Vector3 rotationAxis, float rotationAngle, Vector3 scale, Color tint); // Draw a model with extended parameters
+RLAPI void DrawModelMat(Model model, Vector3 position, float scale, Color tint, Material* _mat);
 RLAPI void DrawModelWires(Model model, Vector3 position, float scale, Color tint);          // Draw a model wires (with texture if set)
 RLAPI void DrawModelWiresEx(Model model, Vector3 position, Vector3 rotationAxis, float rotationAngle, Vector3 scale, Color tint); // Draw a model wires (with texture if set) with extended parameters
 RLAPI void DrawBoundingBox(BoundingBox box, Color color);                                   // Draw bounding box (wires)
